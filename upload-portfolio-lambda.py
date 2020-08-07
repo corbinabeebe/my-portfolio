29bfa2bda9e1d295b2087930ed209e2f35a4705d
Corbin Beebe <corbinbeebe@gmail.com>
2020-08-07 11:38:11 -0700

    matched script in upload=portfolio-lambda.py to script in laws lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,26 +1,35 @@
+import json
 import boto3
 import io
 import zipfile
 import mimetypes
 
-#sets resource to aws s3
-s3 = boto3.resource('s3')
 
-#set s3 bucket and build bucket
-portfolio_bucket = s3.Bucket('portfolio.cbeebe.net')
-build_bucket = s3.Bucket("portfoliobuild.cbeebe.net")
+def lambda_handler(event, context):
 
-#set portfolio_zip to byte io in memory file
-portfolio_zip = io.BytesIO()
+    #sets resource to aws s3
+    s3 = boto3.resource('s3')
 
-#downloads zipfile
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    #set s3 bucket and build bucket
+    portfolio_bucket = s3.Bucket('portfolio.cbeebe.net')
+    build_bucket = s3.Bucket("portfoliobuild.cbeebe.net")
 
-#use zipfile module to extract, upload and set ACL
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={
-            'ContentType':mimetypes.guess_type(nm, strict=True)[0],
-            'ACL' :'public-read'})
+    #set portfolio_zip to byte io in memory file
+    portfolio_zip = io.BytesIO()
+
+    #downloads zipfile
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+    #use zipfile module to extract, upload and set ACL
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={
+                'ContentType':mimetypes.guess_type(nm, strict=True)[0],
+                'ACL' :'public-read'})
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
