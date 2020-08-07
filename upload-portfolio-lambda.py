e7caf3fe6fd8580c4fcab3b330bbde32f3e8667c
Corbin Beebe <corbinbeebe@gmail.com>
2020-08-07 13:31:13 -0700

    added codebuild put job success call to lambda upload

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -10,18 +10,30 @@ def lambda_handler(event, context):
     topic = sns.Topic('arn:aws:sns:us-east-1:488383729927:deployPortfolioTopic')
 
     try:
+        job = event.get("CodePipeline.job")
+
+        location = {
+            "bucketName": "portfoliobuild.cbeebe.net",
+            "objectKey": 'portfoliobuild.zip'
+        }
+        if job:
+            for artifact in job['data']['inputArtifacts']:
+                if artifact['name'] == "BuildArtifact":
+                    location = artifact["location"]["s3Location"]
+
+        print(f"Building portfolio from {str(location)}")
         #sets resource to aws s3
         s3 = boto3.resource('s3')
-        
+
         #set s3 bucket and build bucket
         portfolio_bucket = s3.Bucket('portfolio.cbeebe.net')
-        build_bucket = s3.Bucket("portfoliobuild.cbeebe.net")
+        build_bucket = s3.Bucket(location['bucketName'])
 
         #set portfolio_zip to byte io in memory file
         portfolio_zip = io.BytesIO()
-
+        
         #downloads zipfile
-        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        build_bucket.download_fileobj(location['objectKey'], portfolio_zip)
 
         #use zipfile module to extract, upload and set ACL
         with zipfile.ZipFile(portfolio_zip) as myzip:
@@ -34,6 +46,9 @@ def lambda_handler(event, context):
 
         print("Job done!")
         topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed successfully!")
+        if job:
+            codepipeline = boto3.client('codepipeline')
+            codepipeline.put_job_success_result(jobID=job['id'])
     except:
         topic.publish(Subject="Portfolio Deploy Failed", Message="Something terrible happened!! Portfolio deployment crashed and burned!")
         raise
