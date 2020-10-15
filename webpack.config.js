751cbf5535e83a9682304307ba489da703c795e3
Corbin Beebe <corbinbeebe@gmail.com>
2020-10-15 07:43:44 -0700

    updated resume to reflect new role with Apolis at EPRI

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,9 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx?$/,
+                test: [/\.jsx?$/],
                 exclude: [
-                    /node_modules/
+                    path.resolve(__dirname, "/node_modules/")
                 ],
                 loader: "babel-loader",
                 options: {
