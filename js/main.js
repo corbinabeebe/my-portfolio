f8e185b3a8d9aaeaf203df4fea233e3dd4ee047f
Corbin Beebe <corbinbeebe@gmail.com>
2020-10-26 14:18:54 -0700

    changed multiline string formatting

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ const myWork = [
   {
     title: "Book Review Web App",
     href: "https://github.com/corbinabeebe/flaskapps/tree/master/project1",
-    desc: ``` Book review web application. Uses flask micro service framework connected to a Heroku/Postgres
+    desc: `Book review web application. Uses flask micro service framework connected to a Heroku/Postgres
             database via SQLAlchemy. Allows user to create a login, authenticate login information, search for books, leave reviews on books,
-            and sends a http requests to Goodreads.com to show review information and count (Python, HTML5, CSS, Bootstrap, Flask)```,
+            and sends a http requests to Goodreads.com to show review information and count (Python, HTML5, CSS, Bootstrap, Flask)`,
     image: {
       desc: "Screenshot of book review web app",
       src: "images/book_review_app.jpg",
@@ -18,10 +18,10 @@ const myWork = [
   {
     title: "Serverless Portfolio",
     href: "https://portfolio.cbeebe.net",
-    desc: ```Serverless Portfolio of projects built with JavaScript. Uses React, Babel for primary structure of portfolio.
+    desc: `Serverless Portfolio of projects built with JavaScript. Uses React, Babel for primary structure of portfolio.
             Uses Chai, Mocha, Jest and Enzyme for testing. Portfolio is then bundled with webpack and uploaded to AWS via a python a boto3
             lambda script. Portfolio is tracked on AWS with Code Build and Code Pipeline (Python, JavaScript, React.js, Babel, AWS: Route56,
-            CloudFront, Lambda, Codebuild```,
+            CloudFront, Lambda, Codebuild)`,
     image: {
       desc: "Image of Serverless portfolio",
       src: "images/portfoliopicture.jpg",
@@ -32,9 +32,9 @@ const myWork = [
     title: "Password Generator Application",
     href:
       "https://github.com/corbinabeebe/Android_Apps/tree/master/PasswordGenerator",
-    desc: ```Android app that allows the user to select the length of the password they want generated with a seek
+    desc: `Android app that allows the user to select the length of the password they want generated with a seek
             bar. User can then click the generate button to generate a random password of desired length. Password meets most password policy
-            requirements (Java, Android, XML)```,
+            requirements (Java, Android, XML)`,
     image: {
       desc: "image of password generator app",
       src: "images/PasswordGenerator.jpg",
